30d4cb5d7ed51ef0ba6b3b9614b8eb7bf1165b96
Henrietteode <henriette.odegard.000@gmail.com>
2023-10-23 09:40:42 +0200

    2 Oppgaver committed

---
+++ html_Form/color.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn");
+
+function myFunction(event) {
+    
+
+}
+
+function changecolor() {
+
+    const color = document.getElementById('color').value
+
+    document.body.style.backgroundColor = color
+
+}
